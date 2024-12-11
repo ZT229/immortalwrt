35246d6ada1d9643e4bbce038747987fb3b37c46
TAO ZENG <54100752+ZT229@users.noreply.github.com>
2024-12-11 09:56:46 +0800

    Update target.mk

--- target/linux/mediatek/filogic/target.mk
+++ target/linux/mediatek/filogic/target.mk
@@ -4,7 +4,7 @@ BOARDNAME:=Filogic 8x0 (MT798x)
 CPU_TYPE:=cortex-a53
 DEFAULT_PACKAGES += fitblk kmod-phy-aquantia kmod-crypto-hw-safexcel wpad-openssl uboot-envtools bridger
 KERNELNAME:=Image dtbs
-DEFAULT_PROFILE:=openwrt_one
+DEFAULT_PROFILE:=h3c_magic-nx30-pro
 
 define Target/Description
 	Build firmware images for MediaTek Filogic ARM based boards.
