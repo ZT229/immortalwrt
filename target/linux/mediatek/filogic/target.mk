b96a96914d48c59604bbf4c136dedcf024243455
ZT229 <2432702069@qq.com>
2024-12-18 20:13:03 +0800

    添加编译优化

--- target/linux/mediatek/filogic/target.mk
+++ target/linux/mediatek/filogic/target.mk
@@ -2,6 +2,8 @@ ARCH:=aarch64
 SUBTARGET:=filogic
 BOARDNAME:=Filogic 8x0 (MT798x)
 CPU_TYPE:=cortex-a53
+TARGET_CFLAGS += -O3 -pipe -funroll-loops -mcpu=cortex-a53
+TARGET_LDFLAGS += -Wl,-O2
 DEFAULT_PACKAGES += fitblk kmod-phy-aquantia kmod-crypto-hw-safexcel wpad-openssl uboot-envtools bridger
 KERNELNAME:=Image dtbs
 DEFAULT_PROFILE:=h3c_magic-nx30-pro
